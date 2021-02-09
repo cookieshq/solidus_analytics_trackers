4d8acee5a9178fa99e921c434fc8bc30360828e5
James Reading <james@cookieshq.co.uk>
2021-02-09 14:01:30 +0000

    Fix helper not available in checkout controller

---
+++ app/controllers/spree_analytics_trackers/checkout_controller_decorator.rb
@@ -0,0 +1,13 @@
+module SpreeAnalyticsTrackers
+  module CheckoutControllerDecorator
+    def self.included(base)
+      base.include ::Spree::BaseHelper
+
+      base.helper 'spree/trackers'
+    end
+  end
+end
+
+if defined?(Spree::CheckoutController)
+  ::Spree::CheckoutController.include(::SpreeAnalyticsTrackers::CheckoutControllerDecorator)
+end
