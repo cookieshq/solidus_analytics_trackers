6b1fe1990982ddcd196b6a3bd32f9e5e41ebab0a
James Reading <james@cookieshq.co.uk>
2021-02-09 14:25:13 +0000

    Add GA initializer and page view (untested)

--- app/overrides/add_google_analytics_to_spree_application.rb
+++ app/overrides/add_google_analytics_to_spree_application.rb
@@ -1,9 +1,6 @@
-if Gem.loaded_specs['spree_core'].version >= Gem::Version.create('3.5.0.alpha')
-  Deface::Override.new(
-    virtual_path: 'spree/shared/_head',
-    name: 'add_google_analytics_initializer_to_spree_application',
-    insert_after: 'title',
-    partial: 'spree/shared/trackers/google_analytics/initializer.js',
-    original: 'cfa30a2831d9a41394c03229cd28b3c7eee69585'
-  )
-end
+Deface::Override.new(
+  virtual_path: 'spree/layouts/spree_application',
+  name: 'add_google_analytics_initializer_to_spree_application',
+  insert_bottom: '[data-hook="inside_head"]',
+  partial: 'spree/shared/trackers/google_analytics/initializer.js'
+)
